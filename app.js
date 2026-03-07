1dc8004f6b5e5d576f26aa9284e39094357b585c
Air <air@Air-di-Air.homenet.telecomitalia.it>
2026-03-07 18:40:54 +0100

    Feat: Updated app to v2.0

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>DevOps Pipeline Successful!</h1><p>Running on AWS Elastic Beanstalk.</p>');
+  // We are changing the text here
+  res.send('<h1>DevOps Pipeline v2.0!</h1><p>Automatic Deployment is working perfectly on AWS.</p>');
 });
 
 app.listen(port, () => {
